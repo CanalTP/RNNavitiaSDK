cb07dd03501d9fe9989b9bb14b89001fd4e91cfc
Thomas Noury <thomas.noury@kisio.org>
2017-07-03 15:32:51 +0200

    Bridge version fixed and static SDK dependency version

--- tasks/ios.rb
+++ tasks/ios.rb
@@ -18,26 +18,15 @@ target.add_file_references([
   project.main_group.new_file('../node_modules/react-native-navitia-sdk/RNNavitiaSDK/RNNavitiaSDK.m')
 ])
 
-source_files = target.source_build_phase.files.to_a.map do |pbx_build_file|
-  pbx_build_file.file_ref.real_path.to_s
-end
-
-puts 'Source files in Xcode project:'
-puts source_files
-
 project.save(project_path)
 
 if not File.exist?('./ios/Podfile') then
   system "cd ios && pod init"
 end
 
-packageFile = File.open('./node_modules/react-native-navitia-sdk/package.json').read
-packageData = JSON.parse(packageFile.to_s)
-version = packageData['version']
-
 File.open('./ios/Podfile', 'a+') do |file|
   file.puts "\r" + "use_frameworks!"
-  file.puts "\r" + "pod 'NavitiaSDK', '" + version + "'"
+  file.puts "\r" + "pod 'NavitiaSDK', '0.1'"
 end
 
 system "cd ios && pod install"
